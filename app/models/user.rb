5453f7662ae2bd6bd490e8c982977ffa3e5707c2
hrmyg <75000880+hrmyg@users.noreply.github.com>
2020-12-21 20:30:22 +0900

    バリデーションのコードのDRY

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'} do
-   validates :password
+  with_options presence: true do 
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角漢字、ひらがな、カタカナで入力してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角漢字、ひらがな、カタカナで入力してください' }
+    validates :last_name_kana, format: { with: /\A([ァ-ン]|ー)+$\z/, message: 'は全角カタカナで入力してください'}
+    validates :first_name_kana, format: { with: /\A([ァ-ン]|ー)+$\z/, message: 'は全角カタカナで入力してください'}
+    validates :birthday
   end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角漢字、ひらがな、カタカナで入力してください' } do
-    validates :last_name
-    validates :first_name
-  end
-  with_options presence: true, format: { with: /\A([ァ-ン]|ー)+$\z/, message: 'は全角カタカナで入力してください'} do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-  validates :birthday, presence: true
 end
