fc2b2442e97dbae54dc0a00f428f20fc7afa206a
hrmyg <75000880+hrmyg@users.noreply.github.com>
2020-12-21 19:20:52 +0900

    UserUserモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'} do
+   validates :password
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角漢字、ひらがな、カタカナで入力してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A([ァ-ン]|ー)+$\z/, message: 'は全角カタカナで入力してください'} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
   validates :birthday, presence: true
 end
