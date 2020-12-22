53f622c985ded09fe9aa72f431fb947b28467792
hrmyg <75000880+hrmyg@users.noreply.github.com>
2020-12-22 17:08:24 +0900

    商品出品機能（データ保存）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.find(params[:id])
+    if @item.create
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :state_id, :shipping_cost_id, :prefecture, :shipping_day_id, :image).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :category, :state, :shipping_cost, :prefecture, :shipping_day
 
-  validates :name, :explanation, :image, presence: true
+  validates :name, :explanation, :price, :image, presence: true
 
   validates :category_id, :state_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, numericality: { other_then: 1 }
 end
