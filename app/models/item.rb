259a6035737a60b057348f1da9b5cc4f4b992e8d
hrmyg <75000880+hrmyg@users.noreply.github.com>
2020-12-22 14:46:43 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
   belongs_to :category, :state, :shipping_cost, :prefecture, :shipping_day
 
-  validates :name, :explanation, presence: true
+  validates :name, :explanation, :image, presence: true
 
   validates :category_id, :state_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, numericality: { other_then: 1 }
 end
