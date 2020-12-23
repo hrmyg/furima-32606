af862dbdea9403ac4a22e712f1366e3cf2c8933b
hrmyg <75000880+hrmyg@users.noreply.github.com>
2020-12-23 14:33:58 +0900

    active_hash（修正）

--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -3,7 +3,7 @@ class ShippingDay < ActiveHash::Base
     { id: 1, name: '---'},
     { id: 2, name: '1~2日で配送'},
     { id: 3, name: '2~3日で配送'},
-    { id: 4, naem: '4~7日で配送'}
+    { id: 4, name: '4~7日で配送'}
   ]
 
   include ActiveHash::Associations
